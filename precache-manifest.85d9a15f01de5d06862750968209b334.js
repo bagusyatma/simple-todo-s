436205895abba9c16d91dfe43b080f47e633862b
bagusyatma <bagusyatma1@gmail.com>
2020-05-07 00:38:09 +0700

    Updates

---
+++ precache-manifest.85d9a15f01de5d06862750968209b334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79259154cb3db29b879a7a8cd8b51a9d",
+    "url": "/simple-todo-s/index.html"
+  },
+  {
+    "revision": "a0e7a20c6304412a2c74",
+    "url": "/simple-todo-s/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cabf7eb3c10d035e1a38",
+    "url": "/simple-todo-s/static/js/2.36f4692c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-s/static/js/2.36f4692c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e7a20c6304412a2c74",
+    "url": "/simple-todo-s/static/js/main.54f48e35.chunk.js"
+  },
+  {
+    "revision": "3a45398b901d21bb267a",
+    "url": "/simple-todo-s/static/js/runtime-main.8de6f5dd.js"
+  }
+]);
